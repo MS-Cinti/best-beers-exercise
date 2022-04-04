f1d0c419497d189bf8c94de33bf40ec74e3758da
MS-Cinti <szabadicintia@gmail.com>
2022-04-04 23:53:44 +0200

    added styles to beerspage

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,18 +1,21 @@
-
 fetch ("./dog/data.json")
 .then(response => response.json())
 .then(data => {
     for (const beer of data.cards) {
     document.getElementById('beersPage').insertAdjacentHTML("beforeend", `
-        <div class="cards">
-            <h2>${beer.title}</h2><br>
-            <p>${beer.sub}</p><br>
-            <p>${beer.text}</p><br>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src="./dog/imagesAndVideos/${beer.labelImg}" alt="" style="width:100%;height:100%;object-fit:cover">
+            </div>
+            <div class="flip-card-back">
+                <img src="./dog/imagesAndVideos/${beer.img}.jpg" alt="" style="width:100%;height:100%;object-fit:contain">
+                <h1>${beer.title}</h1> 
+                <p>${beer.sub}</p> 
+                <p>${beer.text}</p>
+            </div>
         </div>
-        `
+    </div>
+`
     )}
 })
-
-//másik megoldások:
-//Szilvitől
-//Krisztitől
